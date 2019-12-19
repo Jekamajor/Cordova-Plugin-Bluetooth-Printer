e1160dba3e349c328363b68a8e820a189d060ed6
unknown <C:\Program Files\The Bat!\mail>
2019-12-20 01:10:49 +0200

    1

--- www/BluetoothPrinter.js
+++ www/BluetoothPrinter.js
@@ -6,28 +6,40 @@ var BTPrinter = {
    },
    test: function(fnSuccess, fnError){
       exec(fnSuccess, fnError, "BluetoothPrinter", "test", ['abc']);
-   },
-   list: function(fnSuccess, fnError){
-      exec(fnSuccess, fnError, "BluetoothPrinter", "list", []);
-   },
-   connect: function(fnSuccess, fnError, name){
-      exec(fnSuccess, fnError, "BluetoothPrinter", "connect", [name]);
-   },
-   disconnect: function(fnSuccess, fnError){
-      exec(fnSuccess, fnError, "BluetoothPrinter", "disconnect", []);
-   },
-   print: function(fnSuccess, fnError, str){
-      exec(fnSuccess, fnError, "BluetoothPrinter", "print", [str]);
-   },
-   printText: function(fnSuccess, fnError, str){
-      exec(fnSuccess, fnError, "BluetoothPrinter", "printText", [str]);
-   },
-    printImage: function(fnSuccess, fnError, str){
-      exec(fnSuccess, fnError, "BluetoothPrinter", "printImage", [str]);
+   },	
+    status: function (fnSuccess, fnError) {
+        exec(fnSuccess, fnError, "BluetoothPrinter", "status", []);
     },
-   printPOSCommand: function(fnSuccess, fnError, str){
-      exec(fnSuccess, fnError, "BluetoothPrinter", "printPOSCommand", [str]);
-   }
+    list: function (fnSuccess, fnError) {
+        exec(fnSuccess, fnError, "BluetoothPrinter", "list", []);
+    },
+    connect: function (fnSuccess, fnError, name) {
+        exec(fnSuccess, fnError, "BluetoothPrinter", "connect", [name]);
+    },
+    disconnect: function (fnSuccess, fnError) {
+        exec(fnSuccess, fnError, "BluetoothPrinter", "disconnect", []);
+    },
+    printText: function (fnSuccess, fnError, str) {
+        exec(fnSuccess, fnError, "BluetoothPrinter", "printText", [str]);
+    },
+    printTextSizeAlign: function (fnSuccess, fnError, str, size, align) {
+        exec(fnSuccess, fnError, "BluetoothPrinter", "printTextSizeAlign", [str, size, align]);
+    },
+    printTitle: function (fnSuccess, fnError, str, size, align) {
+        exec(fnSuccess, fnError, "BluetoothPrinter", "printTitle", [str, size, align]);
+    },
+    printImageUrl: function (fnSuccess, fnError, str, align) {
+        exec(fnSuccess, fnError, "BluetoothPrinter", "printImageUrl", [str, align]);
+    },
+    printBase64: function (fnSuccess, fnError, str, align) {
+        exec(fnSuccess, fnError, "BluetoothPrinter", "printBase64", [str, align]);
+    },
+    printPOSCommand: function (fnSuccess, fnError, str) {
+        exec(fnSuccess, fnError, "BluetoothPrinter", "printPOSCommand", [str]);
+    },
+    printQRCode: function (fnSuccess, fnError, str) {
+        exec(fnSuccess, fnError, "BluetoothPrinter", "printQRCode", [str]);
+    }
 };
 
 module.exports = BTPrinter;
